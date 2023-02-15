e6be9f36507968c4f4f95ab4948400c96fdeb366
chaen-takumi <chaen.ta1227@gmail.com>
2023-02-15 19:22:40 +0900

    update

--- app/src/main/java/com/example/name_app/MainActivity.kt
+++ app/src/main/java/com/example/name_app/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,11 @@ class MainActivity : AppCompatActivity() {
 
         //画面遷移
         btnStart.setOnClickListener {
+            if (et.text.toString() == ("")) {
+                Toast.makeText(this, "何か書いて", Toast.LENGTH_LONG).show()
+            }
+
+        } else {
             val intent = Intent(this, SecondActivity::class.java)
             //値の受け渡し
             intent.putExtra("MY_NAME", et.text.toString())
